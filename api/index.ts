1c4daf26804ed4fe3a9f9e672a375aa81c5ebb46
dba18714 <dba18714@gmail.com>
2025-06-29 19:15:44 +0800

    注释掉了服务器启动的监听代码

--- api/index.ts
+++ api/index.ts
@@ -124,6 +124,6 @@ app.get('/allUsers', async (req, res) => {
 	}
 });
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+// app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 module.exports = app;
